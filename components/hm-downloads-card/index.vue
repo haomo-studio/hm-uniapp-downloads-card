d28b82752c85a21e9bfbc08dda8f26233c751b59
李日胜 <lirisheng@haomo-studio.com>
2020-03-13 16:57:40 +0800

    init

---
+++ components/hm-downloads-card/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hm-downloads-card">
+    <div class="container">
+      <div class="downloadDetail">
+        <div class="downloadDetail-container">
+          <text class="count">{{ options.count }}</text>
+          <text class="downloads">{{ options.downloads }}</text>
+        </div>
+      </div>
+      <image class="downloadImage" :src="options.downloadImage" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HmDownloadsCard',
+  props: {
+    dataId: {
+      type: String,
+      default: 'hm-downloads-card'
+    },
+    options: {
+      type: Object,
+      default: function() {
+        return {
+          count: '299.674',
+          downloads: '下载量',
+          downloadImage:
+            './images/img_24402_0_0.png'
+        };
+      }
+    }
+  },
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+<style>
+@import './index.response.css';
+</style>
